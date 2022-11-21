b1e918808282117bd17eea86d20537e1d05dd31e
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-11-21 19:53:22 +0800

    Update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,25 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
+import NextCors from "nextjs-cors"
 
 type QuestionButtonProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 const QuestionButton = ({ children }: QuestionButtonProps) => {
   return (
-    <button
-      className="flex flex-row justify-center bg-slate-500 px-4 py-4 rounded-2xl shadow-lg text-slate-300">
+    <button className="flex flex-row justify-center rounded-2xl bg-slate-500 px-4 py-4 text-slate-300 shadow-lg">
       {children}
     </button>
-
-  )
-}
+  );
+};
 
 type QuestionOneProps = {
-  children?: ReactNode
-}
+  children?: ReactNode;
+};
 
 const QuestionOne = ({ children }: QuestionOneProps) => {
   return (
     <div>
-      <div className="font-bold text-7xl pb-8">
-        Question #1:
-      </div>
+      <div className="pb-8 text-7xl font-bold">Question #1:</div>
       <h2 className="text-4xl font-semibold">
         We’d like to know more about you before we begin.
         <br />
@@ -31,26 +28,20 @@ const QuestionOne = ({ children }: QuestionOneProps) => {
           What is your relationship with Peace Center?
         </i>
         <div className="grid grid-cols-2 gap-4 pt-8">
-          <QuestionButton>
-            I visit it frequently.
-          </QuestionButton>
-          <QuestionButton>
-            I visit it occassionally.
-          </QuestionButton>
-          <QuestionButton>
-            I visit it rarely.
-          </QuestionButton>
+          <div >
+            <QuestionButton>I visit it frequently.</QuestionButton>
+          </div>
+          <QuestionButton>I visit it occassionally.</QuestionButton>
+          <QuestionButton>I visit it rarely.</QuestionButton>
         </div>
       </h2>
     </div>
-
-  )
-}
+  );
+};
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <div className="flex h-screen flex-col items-center justify-center">
       <QuestionOne />
     </div>
-
-  )
+  );
 }
