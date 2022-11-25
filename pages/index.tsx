ccc934ff5d244ce9c7b8cb8ee5baa79a7c7aec2d
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-11-25 20:20:10 +0800

    add payload for img2img

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,54 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import QuestionButton from "../components/questionButton";
-import Image from 'next/image'
-
+import Image from "next/image";
+import useSWR from "swr";
 
 const QuestionOne = () => {
+  const [data, setData] = useState<any>();
+  const [imgbase64, setImgBase64] = useState<any>();
 
-  const [data, setData] = useState<any>()
+  const image = "/images/peace-center-2.jpg";
+
+  const getBase64StringFromDataURL = (dataURL: any) =>
+    dataURL.replace("data:", "").replace(/^.+,/, "");
+
+  useEffect(() => {
+    fetch(image)
+      .then((res) => res.blob())
+      .then((blob) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          const base64 = getBase64StringFromDataURL(reader.result);
+          setImgBase64(base64);
+        };
+        reader.readAsDataURL(blob);
+      });
+  }, []);
 
   const payload = {
-    "prompt": 'faded and grainy wide angle shot of a vintage 80s interior, futuristic, soft and dreamy lighting, cyberpunk 2077 aesthetic'
-  }
+    init_images: [`data:image/png;base64,${imgbase64}`],
+    include_init_images: true,
+    denoising_strength: 0.47,
+    steps: 20,
+    cfg_scale: 9,
+    prompt:
+      "faded and grainy wide angle shot of a vintage 80s interior, futuristic, soft and dreamy lighting, cyberpunk 2077 aesthetic",
+  };
 
   function GenerateImage() {
     const options = {
-      method: 'POST',
-      headers: { 'content-type': 'application/json', accept: 'application/json' },
-      body: JSON.stringify(payload)
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        accept: "application/json",
+      },
+      body: JSON.stringify(payload),
     };
 
-    fetch('http://localhost:7860/sdapi/v1/txt2img', options)
-      .then(response => response.json())
-      .then(response => setData(response))
-      .then(response => console.log(response))
+    fetch("http://localhost:7860/sdapi/v1/img2img", options)
+      .then((response) => response.json())
+      .then((response) => setData(response))
+      .then((response) => console.log(response));
   }
 
   return (
@@ -35,21 +62,36 @@ const QuestionOne = () => {
           What is your relationship with Peace Center?
         </i>
         <div className="grid grid-cols-2 gap-4 pt-8">
-          <QuestionButton postData={GenerateImage}>I visit it frequently.</QuestionButton>
+          <QuestionButton postData={GenerateImage}>
+            I visit it frequently.
+          </QuestionButton>
           <QuestionButton>I visit it occassionally.</QuestionButton>
           <QuestionButton>I visit it rarely.</QuestionButton>
         </div>
         <div className="w-[250px]">
-          {
-            (!data) ? null || undefined : <Image src={`data:image/png;base64, ${data?.images}`} alt="SD image" width={512} height={512} />
-          }
+          {!data ? (
+            null || undefined
+          ) : (
+            <Image
+              src={`data:image/png;base64, ${data?.images}`}
+              alt="SD image"
+              width={512}
+              height={512}
+            />
+          )}
         </div>
-      </h2 >
+      </h2>
     </>
-  )
-}
+  );
+};
 
 export default function Home() {
+  const fetcher = (url: RequestInfo | URL) =>
+    fetch(url).then((res) => res.json());
+
+  const { data } = useSWR("api/getimageblob", fetcher);
+  console.log(data);
+
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <QuestionOne />
