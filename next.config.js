2e0f4bb8d5c83d1f98ed1af40381ec5679cac95c
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-11-25 20:20:24 +0800

    Update nextconfig and packages

--- next.config.js
+++ next.config.js
@@ -2,19 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname
+  }
 };
-
 
 
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/:path*',
-  //       destination: 'http://127.0.0.1:7860/:path*'
-  //     }
-  //   ]
-  // }
+
 }
