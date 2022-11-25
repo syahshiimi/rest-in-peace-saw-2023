ab12ef513c17e1908393e362428ce63703d2f4eb
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-11-25 20:25:59 +0800

    Styling

--- pages/api/getimageblob.ts
+++ pages/api/getimageblob.ts
@@ -1,11 +1,16 @@
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import path from "path";
 
-export default function getImageBlob(req: any, res: any) {
-    const dirRelativeToPublicFolder = 'images'
-    const dir = path.resolve('./public', dirRelativeToPublicFolder);
-    const filenames = fs.readdirSync(dir);
-    const images = filenames.map(name => path.join('/', dirRelativeToPublicFolder, name))
-    res.statusCode = 200
-    return res.json(images);
+export default function getImageBlob(
+  req: any,
+  res: { statusCode: number; json: (arg0: string[]) => any }
+) {
+  const dirRelativeToPublicFolder = "images";
+  const dir = path.resolve("./public", dirRelativeToPublicFolder);
+  const filenames = fs.readdirSync(dir);
+  const images = filenames.map((name) =>
+    path.join("/", dirRelativeToPublicFolder, name)
+  );
+  res.statusCode = 200;
+  return res.json(images);
 }
