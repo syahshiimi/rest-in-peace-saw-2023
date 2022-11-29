f242b9a9bc0d9e1200bf26fdd395df7c59affbe5
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-11-30 02:06:50 +0800

    Added prompt generation pipepline

--- components/questionButton.tsx
+++ components/questionButton.tsx
@@ -3,13 +3,17 @@ import { type Dispatch, ReactNode, SetStateAction } from "react"
 interface QuestionButtonProps {
     children?: ReactNode
     isClick?: string | undefined
+    basePrompt?: string | undefined
     setIsClick?: Dispatch<SetStateAction<string>>
+    setBasePrompt?: Dispatch<SetStateAction<any>>
 }
 
 const QuestionButton = ({
     children,
     isClick,
     setIsClick,
+    basePrompt,
+    setBasePrompt,
 }: QuestionButtonProps) => {
     const setBool = () => {
         if (setIsClick != undefined) {
@@ -22,10 +26,22 @@ const QuestionButton = ({
             }
         }
     }
+
+    const setPrompt = () => {
+        if (setBasePrompt != undefined) {
+            setBasePrompt(basePrompt)
+        }
+    }
+
+    const onClick = () => {
+        setBool()
+        setPrompt()
+    }
+
     return (
         <button
             className="flex max-w-xl flex-row items-center justify-center rounded-2xl bg-yellow-800 px-4 py-4 text-lg text-slate-300 shadow-lg"
-            onClick={setBool}
+            onClick={() => onClick()}
         >
             {children}
         </button>
