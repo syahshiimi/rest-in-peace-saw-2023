7f10fd68c7ef8335f12e45328b7fb37dae2c641a
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-11-30 01:29:32 +0800

    Update button to include inline conditions

--- components/questionButton.tsx
+++ components/questionButton.tsx
@@ -1,19 +1,33 @@
-import { ReactNode } from "react";
+import { type Dispatch, ReactNode, SetStateAction } from "react"
 
-type QuestionButtonProps = {
-  children?: ReactNode;
-  postData?: () => void;
-};
+interface QuestionButtonProps {
+    children?: ReactNode
+    isClick?: string | undefined
+    setIsClick?: Dispatch<SetStateAction<string>>
+}
 
-const QuestionButton = ({ children, postData }: QuestionButtonProps) => {
-  return (
-    <button
-      className="flex flex-row justify-center rounded-2xl bg-slate-500 px-4 py-4 text-slate-300 shadow-lg"
-      onClick={postData}
-    >
-      {children}
-    </button>
-  );
-};
+const QuestionButton = ({
+    children,
+    isClick,
+    setIsClick,
+}: QuestionButtonProps) => {
+    const setBool = () => {
+        if (setIsClick != undefined) {
+            if (isClick == "one") {
+                setIsClick("two")
+            } else if (isClick == "two") {
+                setIsClick("three")
+            }
+        }
+    }
+    return (
+        <button
+            className="flex max-w-xl flex-row items-center justify-center rounded-2xl bg-slate-500 px-4 py-4 text-slate-300 shadow-lg"
+            onClick={setBool}
+        >
+            {children}
+        </button>
+    )
+}
 
-export default QuestionButton;
+export default QuestionButton
