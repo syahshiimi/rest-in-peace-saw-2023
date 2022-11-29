fa7ee63a4467b00ed45609c7a7795b82cb857d99
syahshiimi <syahrlanuar95@gmail.com>
2022-11-29 18:22:23 +0800

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,17 @@
-import { ReactNode, useEffect, useState } from "react"
+import { ReactNode, useEffect, useRef, useState } from "react"
 import QuestionButton from "../components/questionButton"
 import Image from "next/image"
 import useSWR from "swr"
 
-const QuestionOne = () => {
-    const [data, setData] = useState<any>()
-    const [imgbase64, setImgBase64] = useState<any>()
+const getBase64StringFromDataURL = (dataURL: any) =>
+    dataURL.replace("data:", "").replace(/^.+,/, "")
 
-    const image = "/images/peace-center-2.jpg"
+interface QuestionOneProps {
+    imgbase64?: string
+}
 
-    const getBase64StringFromDataURL = (dataURL: any) =>
-        dataURL.replace("data:", "").replace(/^.+,/, "")
-
-    useEffect(() => {
-        fetch(image)
-            .then((res) => res.blob())
-            .then((blob) => {
-                const reader = new FileReader()
-                reader.onloadend = () => {
-                    const base64 = getBase64StringFromDataURL(reader.result)
-                    setImgBase64(base64)
-                }
-                reader.readAsDataURL(blob)
-            })
-    }, [])
+const QuestionOne = ({ imgbase64 }: QuestionOneProps) => {
+    const [payloadData, setData] = useState<any>()
 
     const payload = {
         init_images: [`data:image/png;base64,${imgbase64}`],
@@ -44,7 +32,7 @@ const QuestionOne = () => {
             body: JSON.stringify(payload),
         }
 
-        fetch("http://localhost:7860/sdapi/v1/img2img", options)
+        fetch("http://192.168.1.140:7860/sdapi/v1/img2img", options)
             .then((response) => response.json())
             .then((response) => setData(response))
             .then((response) => console.log(response))
@@ -68,11 +56,11 @@ const QuestionOne = () => {
                     <QuestionButton>I visit it rarely.</QuestionButton>
                 </div>
                 <div className="w-[250px]">
-                    {!data ? (
+                    {!payloadData ? (
                         null || undefined
                     ) : (
                         <Image
-                            src={`data:image/png;base64, ${data?.images}`}
+                            src={`data:image/png;base64, ${payloadData?.images}`}
                             alt="SD image"
                             width={512}
                             height={512}
@@ -84,16 +72,42 @@ const QuestionOne = () => {
     )
 }
 
+function randomIntFromInterval(min: number, max: number) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+const rndInt = randomIntFromInterval(1, 10)
+console.log(rndInt)
+
+
 export default function Home() {
+    const [imgbase64, setImgBase64] = useState<any>()
+    const [img, setimg] = useState<any>()
+    // const [imagelist, setImgList] = useState<any>()
+
+    const image = "/images/peace-center-3.jpg"
+
     const fetcher = (url: RequestInfo | URL) =>
         fetch(url).then((res) => res.json())
 
     const { data } = useSWR("api/getimageblob", fetcher)
-    console.log(data)
+
+    useEffect(() => {
+        setimg(data?.[rndInt])
+        fetch(img)
+            .then((res) => res.blob())
+            .then((blob) => {
+                const reader = new FileReader()
+                reader.onloadend = () => {
+                    const base64 = getBase64StringFromDataURL(reader.result)
+                    setImgBase64(base64)
+                }
+                reader.readAsDataURL(blob)
+            })
+    }, [img, imgbase64, data])
 
     return (
         <div className="flex h-screen flex-col items-center justify-center">
-            <QuestionOne />
+            <QuestionOne imgbase64={imgbase64} />
         </div>
     )
 }
