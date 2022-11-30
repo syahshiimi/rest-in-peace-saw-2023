da626d19d5aa3249ce0f2dfda151e629dd014616
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-11-30 22:25:47 +0800

    New lib files

---
+++ src/GenerateImage2Image.ts
@@ -0,0 +1,29 @@
+// import { type GenerateImage2ImageProps } from "../types/GenerateImage2ImageProps"
+import { Dispatch, SetStateAction } from "react"
+
+export interface payloadProps {
+    init_images: string[] | undefined
+    include_init_images: boolean | undefined
+    denoising_strength: number | undefined
+    steps: number | undefined
+    cfg_scale: number | undefined
+    prompt: string | undefined
+}
+
+export default function GenerateImage2Image(
+    payload: payloadProps,
+    setStableImage: Dispatch<SetStateAction<payloadProps>>
+) {
+    const options = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            accept: "application/json",
+        },
+        body: JSON.stringify(payload),
+    }
+
+    fetch("http://127.0.0.1:7860/sdapi/v1/img2img", options)
+        .then((response) => response.json())
+        .then((response) => setStableImage(response))
+}
