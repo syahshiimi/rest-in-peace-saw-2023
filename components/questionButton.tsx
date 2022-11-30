00c14ef73e973beba3e4a3267d2ccce16294144c
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-11-30 16:18:01 +0800

    Added Generate and showImage as state options

--- components/questionButton.tsx
+++ components/questionButton.tsx
@@ -23,6 +23,10 @@ const QuestionButton = ({
                 setIsClick("three")
             } else if (isClick === "three") {
                 setIsClick("four")
+            } else if (isClick === "four") {
+                setIsClick("generate")
+            } else if (isClick === "generate") {
+                setIsClick("showImage")
             }
         }
     }
@@ -40,7 +44,8 @@ const QuestionButton = ({
 
     return (
         <button
-            className="flex max-w-xl flex-row items-center justify-center rounded-2xl bg-yellow-800 px-4 py-4 text-lg text-slate-300 shadow-lg"
+            className="flex max-w-xl flex-row items-center justify-center rounded-2xl bg-yellow-800 px-4 py-4 text-lg text-slate-300 
+            shadow-lg transition-all duration-300 ease-in-out hover:-translate-y-2 hover:bg-yellow-500/80"
             onClick={() => onClick()}
         >
             {children}
