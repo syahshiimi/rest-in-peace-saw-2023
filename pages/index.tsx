b4527336c98e48a0b7944a61757c3d3c067b7b07
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-12-05 20:20:22 +0800

    Initialise tfjs and bodysegmentation library

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,9 @@ import QuestionFour from "../components/QuestionFour"
 import QuestionButton from "../components/questionButton"
 
 import GenerateImage2Image, { payloadProps } from "../src/GenerateImage2Image"
+// import * as tf from "@tensorflow/tfjs-backend-core"
+import "@tensorflow/tfjs-backend-webgl"
+import * as bodySegmentation from "@tensorflow-models/body-segmentation"
 
 const getBase64StringFromDataURL = (dataURL: any) =>
     dataURL.replace("data:", "").replace(/^.+,/, "")
@@ -38,88 +41,151 @@ export default function Home() {
         fetch(url).then((res) => res.json())
     const { data, error } = useSWR("api/getimageblob", fetcher, {})
 
-    let width = 320;    // We will scale the photo width to this
-    let height = 0;     // This will be computed based on the input stream
+    let width = 320 // We will scale the photo width to this
+    let height = 0 // This will be computed based on the input stream
 
-    let streaming = false;
+    let streaming = false
 
-    let video: any = null;
-    let canvas: any = null;
-    let photo: any = null;
-    let startbutton: any = null;
+    let video: any = null
+    let context: any = null
+    let canvas: any = null
+    let photo: any = null
+    let startbutton: any = null
 
-    function clearphoto() {
-        const context = canvas.getContext("2d");
-        context.fillStyle = "#AAA";
-        context.fillRect(0, 0, canvas.width, canvas.height);
+    const clearphoto = () => {
+        const context = canvas.getContext("2d")
+        context.fillStyle = "#AAA"
+        context.fillRect(0, 0, canvas.width, canvas.height)
 
-        const data = canvas.toDataURL("image/png");
-        photo.setAttribute("src", data);
+        const data = canvas.toDataURL("image/png")
+        photo.setAttribute("src", data)
     }
-    function takepicture() {
-        const context = canvas.getContext("2d");
+    const takepicture = () => {
+        const context = canvas.getContext("2d")
         if (width && height) {
-            canvas.width = width;
-            canvas.height = height;
-            context.drawImage(video, 0, 0, width, height);
+            canvas.width = width
+            canvas.height = height
+            context.drawImage(video, 0, 0, width, height)
 
-            const data = canvas.toDataURL("image/png");
-            photo.setAttribute("src", data);
+            const data = canvas.toDataURL("image/png")
+            photo.setAttribute("src", data)
         } else {
-            clearphoto();
+            clearphoto()
         }
     }
 
-    function startup() {
-        video = document.getElementById('video');
-        canvas = document.getElementById('canvas');
-        photo = document.getElementById('photo');
-        startbutton = document.getElementById('startbutton');
+    const startWebcam = () => {
+        video = document.getElementById("video")
+        canvas = document.getElementById("canvas")
+        photo = document.getElementById("photo")
+        startbutton = document.getElementById("startbutton")
         navigator.mediaDevices
             .getUserMedia({ video: true, audio: false })
             .then((stream) => {
-                video.srcObject = stream;
+                video.srcObject = stream
                 //video.play();
             })
             .catch((err) => {
-                console.error(`An error occurred: ${err}`);
-            });
+                console.error(`An error occurred: ${err}`)
+            })
         video.addEventListener(
             "canplay",
             (ev: any) => {
                 if (!streaming) {
-                    height = (video.videoHeight / video.videoWidth) * width;
+                    height = (video.videoHeight / video.videoWidth) * width
 
-                    video.setAttribute("width", width);
-                    video.setAttribute("height", height);
-                    canvas.setAttribute("width", width);
-                    canvas.setAttribute("height", height);
-                    streaming = true;
+                    video.setAttribute("width", width)
+                    video.setAttribute("height", height)
+                    canvas.setAttribute("width", width)
+                    canvas.setAttribute("height", height)
+                    streaming = true
                 }
             },
             false
-        );
-        // startbutton.addEventListener(
-        //     "click",
-        //     (ev) => {
-        //         takepicture();
-        //         // ev.preventDefault();
-        //     },
-        //     false
-        // );
-
-        // clearphoto();
+        )
     }
 
-    const getImageClick = (e: React.FormEvent) => {
+    const takeImageClick = (e: React.FormEvent) => {
         // startbutton.addEventListener("click", (ev) => {
-        takepicture();
-        e.preventDefault();
+        takepicture()
+        e.preventDefault()
         // }, true)
     }
 
+    async function segmenter() {
+        // const img: any = document.getElementById("video")
+        // const model =
+        //     bodySegmentation.SupportedModels.MediaPipeSelfieSegmentation
+        // const segmenterConfig: any = {
+        //     runtime: "mediapipe", // or 'tfjs'
+        //     solutionPath:
+        //         "https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation",
+        //     modelType: "general",
+        // }
+        // const segmenter = await bodySegmentation.createSegmenter(
+        //     model,
+        //     segmenterConfig
+        // )
+        // const people = await segmenter.segmentPeople(img)
+
+        // const foregroundThreshold = 0.5
+        // const backgroundBlurAmount = 3
+        // const edgeBlurAmount = 3
+        // const flipHorizontal = false
+        // const canvas: any = document.getElementById("canvas")
+        // await bodySegmentation.drawBokehEffect(
+        //     canvas,
+        //     img,
+        //     people,
+        //     foregroundThreshold,
+        //     backgroundBlurAmount,
+        //     edgeBlurAmount,
+        //     flipHorizontal
+        // )
+        const img: any = document.getElementById("photo")
+
+        const model =
+            bodySegmentation.SupportedModels.MediaPipeSelfieSegmentation
+        const segmenterConfig: any = {
+            runtime: "mediapipe", // or 'tfjs'
+            solutionPath:
+                "https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation",
+            modelType: "general",
+        }
+        const segmenter = await bodySegmentation.createSegmenter(
+            model,
+            segmenterConfig
+        )
+
+        const segmentation = await segmenter.segmentPeople(img)
+
+        // Convert the segmentation into a mask to darken the background.
+        const foregroundColor = { r: 0, g: 0, b: 0, a: 0 }
+        const backgroundColor = { r: 0, g: 0, b: 0, a: 255 }
+        const backgroundDarkeningMask = await bodySegmentation.toBinaryMask(
+            segmentation,
+            foregroundColor,
+            backgroundColor
+        )
+
+        const opacity = 0.7
+        const maskBlurAmount = 3
+        const flipHorizontal = false
+        const canvas: any = document.getElementById("canvas")
+        // Draw the mask onto the image on a canvas.  With opacity set to 0.7 and
+        // maskBlurAmount set to 3, this will darken the background and blur the
+        // darkened background's edge.
+        await bodySegmentation.drawMask(
+            canvas,
+            img,
+            backgroundDarkeningMask,
+            opacity,
+            maskBlurAmount,
+            flipHorizontal
+        )
+    }
+
     useEffect(() => {
-        startup()
         setimg(data?.[randomInt])
         if (img != undefined) {
             fetch(img)
@@ -215,17 +281,44 @@ export default function Home() {
         <div className="container mx-auto flex min-h-screen max-w-3xl flex-col items-center justify-center">
             {setView()}
             <div className="pt-8">{basePrompt}</div>
-            <div className="flex flex-row">            <div id="camera">
-                <video id="video" autoPlay={true} >Video stream not available.</video>
-                <button id="startbutton" onClick={getImageClick}>Take photo</button>
-            </div>
-                <canvas id="canvas" className="hidden invisible"> </canvas>
+            <button
+                id="startbutton"
+                className="bg-green-500"
+                onClick={startWebcam}
+            >
+                Start Webcam
+            </button>
+
+            <div className="flex flex-row">
+                {" "}
+                <div id="camera">
+                    <video id="video" autoPlay={true}>
+                        Video stream not available.
+                    </video>
+                    <button
+                        id="startbutton"
+                        className="px-2"
+                        onClick={takeImageClick}
+                    >
+                        Take photo
+                    </button>
+                    <button id="startbutton" className="px-2">
+                        Canvas Video
+                    </button>
+                    <button id="startbutton" onClick={segmenter}>
+                        See bokeh picture
+                    </button>
+                </div>
                 <div id="output">
-                    <img id="photo" alt="The screen capture will appear in this box." />
+                    <img
+                        id="photo"
+                        alt="The screen capture will appear in this box."
+                    />
                 </div>
             </div>
-
-
+            <canvas id="canvas" className="">
+                {" "}
+            </canvas>
         </div>
     )
 }
