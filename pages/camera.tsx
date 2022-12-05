9528d91a152648b81b783d9e9a7fde5a409eab93
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-12-06 00:48:46 +0800

    Add camera

--- pages/camera.tsx
+++ pages/camera.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { NextPage } from "next"
 import Webcam from "react-webcam"
 import * as bodySegmentation from "@tensorflow-models/body-segmentation"
@@ -7,6 +7,9 @@ import "@tensorflow/tfjs-backend-webgl"
 const Camera: NextPage = () => {
     const webcamRef = useRef<any>(null)
     const canvasRef = useRef<any>(null)
+    const chromaRef = useRef<any>(null)
+    const imageRef = useRef<any>(null)
+    const [frameData, setFrameData] = useState<any>()
 
     // load bodySegmentation model
     const loadModel = async () => {
@@ -26,8 +29,6 @@ const Camera: NextPage = () => {
             segmenterConfig
         )
 
-        console.log("Segmenter loaded!")
-
         setInterval(() => {
             chromaKey(segmenter)
         }, 1000 / 60)
@@ -40,6 +41,10 @@ const Camera: NextPage = () => {
             webcamRef.current.video.readyState == 4
         ) {
             const video = webcamRef.current.video
+            const canvas = canvasRef.current
+            const canvas2 = chromaRef.current
+
+            // Set Constants based on Webcam Video
             const videoHeight = video.videoHeight
             const videoWidth = video.videoWidth
 
@@ -48,35 +53,72 @@ const Camera: NextPage = () => {
             webcamRef.current.video.height = videoHeight
 
             // set canvas dimensions
-            const canvas = canvasRef.current
             canvasRef.current.height = videoHeight
             canvasRef.current.width = videoWidth
 
+            // Set chromakey canvas dimensions
+            chromaRef.current.height = videoHeight
+            chromaRef.current.width = videoWidth
+
             // Make segmentations
             const person = await segmenter.segmentPeople(video)
-
-            const foregroundThreshold = 0.5
-            const backgroundBlurAmount = 5
-            const edgeBlurAmount = 3
+            const coloredPartImage = await bodySegmentation.toBinaryMask(person)
+            const opacity = 1
             const flipHorizontal = false
-
-            await bodySegmentation.drawBokehEffect(
+            const maskBlurAmount = 2
+            await bodySegmentation.drawMask(
                 canvas,
                 video,
-                person,
-                foregroundThreshold,
-                backgroundBlurAmount,
-                edgeBlurAmount,
+                coloredPartImage,
+                opacity,
+                maskBlurAmount,
                 flipHorizontal
             )
+
+            // Create context to manipulate RGB Values
+            const context = canvas.getContext("2d")
+            const context2 = canvas2.getContext("2d")
+
+            const frame = context.getImageData(
+                0,
+                0,
+                canvas2.width,
+                canvas2.height
+            )
+
+            const data = frame.data
+
+            for (let i = 0; i < data.length; i += 4) {
+                const red = data[i + 0]
+                const green = data[i + 1]
+                const blue = data[i + 2]
+                if (green == 0 && red == 0 && blue == 0) {
+                    data[i + 3] = 0
+                }
+            }
+
+            context2.putImageData(frame, 0, 0)
         }
     }
 
-    console.log(canvasRef)
     return (
         <div className="flex min-h-screen flex-col items-center justify-center">
-            <Webcam ref={webcamRef} className="h-[480px] w-[640px]" />
-            <canvas ref={canvasRef} className="h-[480px] w-[640px]" />
+            <div className="max-w-screen flex flex-row overflow-hidden">
+                {" "}
+                <Webcam
+                    ref={webcamRef}
+                    className="h-[480px] w-[640px] basis-1/3"
+                />
+                <canvas
+                    ref={canvasRef}
+                    className="h-[480px] w-[640px] basis-1/3"
+                />
+                <canvas
+                    ref={chromaRef}
+                    className="h-[480px] w-[640px] basis-1/3 bg-[url('../public/images/peace-center-28.jpg')] bg-cover"
+                />
+            </div>
+
             <button className="bg-red-500" onClick={loadModel}>
                 Get Model
             </button>
