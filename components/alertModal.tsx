2b48bbaeb29d3e1e62c4ba9d13ec8a42f6a0e94b
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-12-20 02:06:08 +0800

    add the modal ref image  canvas

--- components/alertModal.tsx
+++ components/alertModal.tsx
@@ -1,12 +1,19 @@
 import { Dialog, Transition } from "@headlessui/react"
-import { Dispatch, SetStateAction, Fragment } from "react"
+import { Dispatch, SetStateAction, Fragment, MutableRefObject } from "react"
 
 interface IAlertModal {
     isOpen: boolean | undefined
     setIsOpen: Dispatch<SetStateAction<boolean>>
+    modalRef: MutableRefObject<HTMLDivElement>
+    saveImageModal: () => void
 }
 
-const AlertModal = ({ isOpen, setIsOpen }: IAlertModal) => {
+const AlertModal = ({
+    isOpen,
+    setIsOpen,
+    modalRef,
+    saveImageModal,
+}: IAlertModal) => {
     return (
         <Transition
             show={isOpen}
@@ -20,18 +27,18 @@ const AlertModal = ({ isOpen, setIsOpen }: IAlertModal) => {
         >
             <Dialog
                 onClose={() => setIsOpen(false)}
-                className="fixed inset-0 z-50 flex items-center justify-center "
+                className="fixed inset-0 z-50 flex items-end justify-center pb-10 "
             >
                 <div
-                    className="fixed inset-0 bg-black/80 "
+                    className="fixed inset-0 bg-black/90 "
                     aria-hidden="true"
                 />
-                <Dialog.Panel className="z-10 flex flex-col gap-4 rounded-xl bg-amber-600 px-6 py-4">
+                <Dialog.Panel className=" z-30 flex flex-col items-start justify-start gap-4 rounded-xl bg-amber-600 px-6 py-4">
                     <Dialog.Title className="text-2xl font-bold text-slate-300">
                         Picture Saved!
                     </Dialog.Title>
-                    <Dialog.Description className={"text-xl font-medium"}>
-                        Your image has been saved.
+                    <Dialog.Description className="text-xl font-medium">
+                        To save your image, press the save button.
                     </Dialog.Description>
 
                     <p>
@@ -39,10 +46,25 @@ const AlertModal = ({ isOpen, setIsOpen }: IAlertModal) => {
                         your image 💪🏾
                     </p>
 
-                    <div className="flex flex-row gap-8">
-                        <button onClick={() => setIsOpen(false)}>CLOSE</button>
+                    <div className="flex w-full flex-row justify-between gap-8">
+                        <button
+                            onClick={() => setIsOpen(false)}
+                            className="font-semibold"
+                        >
+                            TRY AGAIN
+                        </button>
+                        <button
+                            onClick={saveImageModal}
+                            className="rounded-xl bg-rose-700 px-4 py-3 font-semibold shadow-md"
+                        >
+                            SAVE
+                        </button>
                     </div>
                 </Dialog.Panel>
+                <div
+                    ref={modalRef}
+                    className="absolute left-[50%] top-[50%] z-10 h-full -translate-y-[50%] -translate-x-[50%]  scale-50"
+                />
             </Dialog>
         </Transition>
     )
