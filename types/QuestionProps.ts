f8bfbda4eba4f6a96879084cf08ff6b3d47a5927
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-12-29 22:24:02 +0800

    update question props types to be more strict

--- types/QuestionProps.ts
+++ types/QuestionProps.ts
@@ -2,7 +2,7 @@ import { Dispatch, SetStateAction } from "react"
 
 export interface QuestionProps {
     isClick: "one" | "two" | "three" | "four" | "generate" | "showImage"
-    setIsClick?: Dispatch<SetStateAction<string>>
-    basePrompt?: string
-    setBasePrompt?: Dispatch<SetStateAction<string>>
+    setIsClick: Dispatch<SetStateAction<string>>
+    basePrompt: string
+    setBasePrompt: Dispatch<SetStateAction<string>>
 }
