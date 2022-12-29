4869716c3eeb572d7f4b070fcf2f5d9b2e6ba343
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-12-29 23:14:05 +0800

    started error modal component to improve UI/UX for error handling

---
+++ components/errorModal.tsx
@@ -0,0 +1,8 @@
+import { Dialog, Transition } from "@headlessui/react"
+import { Dispatch, SetStateAction, Fragment, MutableRefObject } from "react"
+
+const ErrorModal = () => {
+    return <div>Error!</div>
+}
+
+export default ErrorModal
